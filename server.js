6e48ad5091db70e0ae02ae0522482c84b1dc27a0
Mitchel <mitchel.carson@gmail.com>
2019-05-23 21:26:35 -0400

    Installed NPM packages

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var handlebars = require("express-handlebars");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
